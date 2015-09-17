235689e46e792f87d6dca34d4327e8f738857730
Darren Ethier <darren@roughsmootheng.in>
2015-09-17 11:30:36 -0400

    add more actions for assisting with testing https://events.codebasehq.com/projects/event-espresso/tickets/7907

--- ee_test_filters_plugin.php
+++ ee_test_filters_plugin.php
@@ -17,17 +17,18 @@
   * This filter tests different date and time formats used for the ticket editor in the event editor.
   * To use:
   * 1. Activate this plugin
-  * 2. Modify the date and time formats in the callback to be various things for testing
-  * 3. Visit the event editor and see that dates are dispayed in the set format, also verify the datepicker uses that format.
-  * 4. Verify that functionality with this format for creating, editing, and saving dates and times in this format works as expected.
+  * 2. Comment out the below filter and callback
+  * 3. Modify the date and time formats in the callback to be various things for testing
+  * 4. Visit the event editor and see that dates are dispayed in the set format, also verify the datepicker uses that format.
+  * 5. Verify that functionality with this format for creating, editing, and saving dates and times in this format works as expected.
   */
-function ee_new_dtt_formats( $formats ) {
+/*function ee_new_dtt_formats( $formats ) {
 	return array(
-		'date' => 'd/m/Y',
+		'date' => 'd F, Y',
 		'time' => 'H:i'
 		);
 }
-add_filter( 'FHEE__espresso_events_Pricing_Hooks___set_hooks_properties__date_format_strings', 'ee_new_dtt_formats' );
+add_filter( 'FHEE__espresso_events_Pricing_Hooks___set_hooks_properties__date_format_strings', 'ee_new_dtt_formats' );/**/
 
 
 /**
@@ -41,7 +42,88 @@ add_filter( 'FHEE__espresso_events_Pricing_Hooks___set_hooks_properties__date_fo
  * 4. Deactivate and reactivate Event Espresso.
  * 5. There should be no errors.
  */
-add_filter( 'FHEE__EE_messenger__get_default_message_types__default_types', function( $default_types, $messenger ) {
+/*add_filter( 'FHEE__EE_messenger__get_default_message_types__default_types', function( $default_types, $messenger ) {
       $default_types[] = 'bogus_message_type';
       return $default_types;
-    }, 10, 2);
+    }, 10, 2);/**/
+
+
+/**
+ * Below actions are for the email messenger wrapper template.  Uncomment to test all the actions when sending an email for the
+ * EE_Registration_message_type.  Emails for other message types should NOT show any changes.
+ */
+/*add_action( 'AHEE__EE_Email_Messenger_main_wrapper_template_head', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Registration_message_type ) {
+		return;
+	} else {
+		echo '<meta property="testing-filter" content="passed">';
+	}
+}, 10, 4 );
+add_action( 'AHEE__EE_Email_Messenger_main_wrapper_template_header', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Registration_message_type ) {
+		return;
+	} else {
+		echo '<header><p>Heya this be the header yo!</p></header>';
+	}
+}, 10, 4 );
+add_action( 'AHEE__EE_Email_Messenger_main_wrapper_template_before_main_body', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Registration_message_type ) {
+		return;
+	} else {
+		echo '<p>HEY YO this be before the main body!</p>';
+	}
+}, 10, 4 );
+add_action( 'AHEE__EE_Email_Messenger_main_wrapper_template_after_main_body', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Registration_message_type ) {
+		return;
+	} else {
+		echo '<p>HEY YO this be after the main body!</p>';
+	}
+}, 10, 4 );
+add_action( 'AHEE__EE_Email_Messenger_main_wrapper_template_footer', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Registration_message_type ) {
+		return;
+	} else {
+		echo '<footer><p>HEY YO this be the footer!</p></footer>';
+	}
+}, 10, 4 );/**/
+
+/**
+ * Below actions are for the html messenger wrapper template.  Uncomment to test all the actions when sending an email for the
+ * EE_Receipt_message_type.  Emails for other message types should NOT show any changes.
+ */
+/*add_action( 'AHEE__EE_Html_Messenger_main_wrapper_template_head', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Receipt_message_type ) {
+		return;
+	} else {
+		echo '<meta property="testing-filter" content="passed">';
+	}
+}, 10, 4 );
+add_action( 'AHEE__EE_Html_Messenger_main_wrapper_template_header', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Receipt_message_type ) {
+		return;
+	} else {
+		echo '<header><p>Heya this be the header yo!</p></header>';
+	}
+}, 10, 4 );
+add_action( 'AHEE__EE_Html_Messenger_main_wrapper_template_before_main_body', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Receipt_message_type ) {
+		return;
+	} else {
+		echo '<p>HEY YO this be before the main body!</p>';
+	}
+}, 10, 4 );
+add_action( 'AHEE__EE_Html_Messenger_main_wrapper_template_after_main_body', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Receipt_message_type ) {
+		return;
+	} else {
+		echo '<p>HEY YO this be after the main body!</p>';
+	}
+}, 10, 4 );
+add_action( 'AHEE__EE_Html_Messenger_main_wrapper_template_footer', function( $message_type, $subject, $from, $main_body ) {
+	if ( ! $message_type instanceof EE_Receipt_message_type ) {
+		return;
+	} else {
+		echo '<footer><p>HEY YO this be the footer!</p></footer>';
+	}
+}, 10, 4 );/**/
